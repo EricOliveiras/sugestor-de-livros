0dd0f3d209846207dc9f47c0d7100b69be18bbd7
Eric Oliveira <oiericoi@hotmail.com>
2025-06-28 12:58:45 -0300

    refactor: update main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./theme.ts"; // 1. IMPORTE NOSSO TEMA
+import { theme } from "./theme.ts";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
